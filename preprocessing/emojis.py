27d0cd708c8c13ec8486ecce7e84cd3dc7872ec8
Marcin <marcin.kardas@gmail.com>
2019-04-12 14:10:54 +0200

    Load files relative to script location

--- preprocessing/emojis.py
+++ preprocessing/emojis.py
@@ -1,12 +1,17 @@
 import lxml.etree as etree
 import re
+import os
 
 EMOJI_LANG = "pl"
+
+def script_relative(filename):
+    script_dir = os.path.dirname(__file__)
+    return os.path.join(script_dir, filename)
 
 def get_emojis(filenames):
     emojis = {}
     for filename in filenames:
-        doc = etree.parse(filename)
+        doc = etree.parse(script_relative(filename))
         for ann in doc.xpath('//annotation[@type="tts"]'):
             emojis[ann.attrib['cp']] = ann.text
     return emojis
